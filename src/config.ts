131dd2375242c3f5b6b5b1aef4a1614d3824d60f
Igor Prokopev <ip@neonlabs.org>
2023-09-11 17:03:23 +0700

    Fixed github tokenlist issue.

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,6 @@ import Bowser from 'bowser'
 
 export const FAUCET_URL = process.env.NODE_ENV === 'development' ? '' : `https://api.${window.location.hostname}`
 
-export const TOKEN_LIST = `https://raw.githubusercontent.com/neonlabsorg/token-list/v4.0.0/tokenlist.json`
-
 export const REQUEST_LIMIT_SEC = 60
 
 export const isMobile = () => {
